5f933b466f4e0c7b8d970cd781ac7e83fb7ee7a3
andreamsilu <msiluandrew2020@gmail.com>
2023-05-06 12:00:32 +0300

    portifolio updated

--- pages/portifolio.php
+++ pages/portifolio.php
@@ -1,10 +1,10 @@
 <?php   include "../header.php" ?>
 <div class="container-fluid">
   <div class="row">
-    <div class="col-md-4">
+    <div class="col-md-5 m-2">
       <div class="card">
         <a href="project1.html" target="_blank">
-          <img class="card-img-top" src="images/pexels-asad-photo-maldives-3293148.jpg" alt="Project 1" 
+          <img class="card-img-top p-5" src="images/pexels-asad-photo-maldives-3293148.jpg" alt="Project 1" 
           style="transition: transform 0.5s ease;
           filter: brightness(70%);
           border-radius: 5px;">
@@ -12,13 +12,15 @@
         <div class="card-body">
           <h5 class="card-title">Project 1</h5>
           <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h6>Time Used: 1 month</h6>
+          <h6>Project Cost: $200</h6>
         </div>
       </div>
     </div>
-    <div class="col-md-4">
+    <div class="col-md-5 m-1 ">
       <div class="card">
         <a href="project2.html" target="_blank">
-          <img class="card-img-top" src="images/pexels-frank-cone-3674987.jpg" alt="Project 2" 
+          <img class="card-img-top p-5" src="images/pexels-frank-cone-3674987.jpg" alt="Project 2" 
           style="transition: transform 0.5s ease;
           filter: brightness(70%);
           border-radius: 5px;">
@@ -26,13 +28,15 @@
         <div class="card-body">
           <h5 class="card-title">Project 2</h5>
           <p class="card-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem.</p>
+          <h6>Time Used: 2 months</h6>
+          <h6>Project Cost: $500</h6>
         </div>
       </div>
     </div>
-    <div class="col-md-4">
+    <div class="col-md-5 m-1">
       <div class="card">
         <a href="project3.html" target="_blank">
-          <img class="card-img-top" src="images/pexels-isabella-mendes-341013.jpg   " alt="Project 3" 
+          <img class="card-img-top p-5" src="images/pexels-isabella-mendes-341013.jpg" alt="Project 3" 
           style="transition: transform 0.5s ease;
           filter: brightness(70%);
           border-radius: 5px;">
@@ -40,6 +44,8 @@
         <div class="card-body">
           <h5 class="card-title">Project 3</h5>
           <p class="card-text">At vero eos et accusamus et iusto odio dignissimos ducimus.</p>
+          <h6>Time Used: 3 weeks</h6>
+          <h6>Project Cost: $300</h6>
         </div>
       </div>
     </div>
