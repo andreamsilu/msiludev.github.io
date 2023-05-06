7b4ef1ee37d2b2de30ee16ec50a502316803afb3
andreamsilu <msiluandrew2020@gmail.com>
2023-05-06 17:48:34 +0300

    footer updated with bootstrap

--- footer.php
+++ footer.php
@@ -1,19 +1,18 @@
-<footer class="bg-dark text-white py-3">
+<footer class="bg-primary text-white py-3">
 	<div class="container">
 		<div class="row">
-			
 			<div class="col-md-6">
-				<ul class="list-inline text-md-right">
-					<li class="list-inline-item"><a href="pages/index.php">Home</a></li>
-					<li class="list-inline-item"><a href="pages/about.php">About</a></li>
-					<li class="list-inline-item"><a href="pages/about.php">Skills</a></li>
-					<li class="list-inline-item"><a href="pages/portifolio.php">Portifolio</a></li>
-					<li class="list-inline-item"><a href="blog/index.php">Blog/a></li>
-					<li class="list-inline-item"><a href="pages/contact.php">Contact</a></li>
+				<ul class="list-inline text-md-right mb-0">
+					<li class="list-inline-item"><a href="pages/index.php" class="text-white">Home</a></li>
+					<li class="list-inline-item"><a href="pages/about.php" class="text-white">About</a></li>
+					<li class="list-inline-item"><a href="pages/skills.php" class="text-white">Skills</a></li>
+					<li class="list-inline-item"><a href="pages/portfolio.php" class="text-white">Portfolio</a></li>
+					<li class="list-inline-item"><a href="blog/index.php" class="text-white">Blog</a></li>
+					<li class="list-inline-item"><a href="pages/contact.php" class="text-white">Contact</a></li>
 				</ul>
 			</div>
-            <div class="col-md-6">
-				<p style = 'text-align:center'>&copy; 2023 Andrew | All Rights Reserved</p>
+			<div class="col-md-6 text-md-right">
+				<p class="mb-0">&copy; 2023 Andrew | All Rights Reserved</p>
 			</div>
 		</div>
 	</div>
@@ -25,7 +24,6 @@
 	}
 	
 	footer a {
-		color: white;
 		transition: color 0.3s ease;
 	}
 	
